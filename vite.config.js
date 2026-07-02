2e5a3dc6704ef9d32de4510fa35dd641030ef0b8
Yogabayusavira <yogabayusavira@gmail.com>
2026-07-03 06:11:57 +0700

    chore: update and regenerate node_modules dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        launcher: resolve(__dirname, 'launcher-overview.html')
-      }
-    }
-  }
+  plugins: [react()],
 })
