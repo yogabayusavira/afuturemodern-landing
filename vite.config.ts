1ae581e920e05118de9e5d2e9aaf59983989ae92
Yogabayusavira <yogabayusavira@gmail.com>
2026-07-20 00:59:51 +0700

    feat: install and integrate @vercel/toolbar into Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vercelToolbar } from '@vercel/toolbar/plugins/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercelToolbar()],
   server: {
     proxy: {
       '/api': {
